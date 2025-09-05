ad4de0048828ec181ad2a875ec98f1d06fde0b05
0101-Victor <rouge.of@gmail.com>
2025-09-05 13:47:42 +0900

    Fix static and database settings for Heroku

--- myproject/settings.py
+++ myproject/settings.py
@@ -155,7 +155,10 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
