55306e9d98aa49c1e3a29918089f0ed2d3fee072
0101-Victor <rouge.of@gmail.com>
2025-09-18 22:42:31 +0900

    Fix signals for safe profile delete

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,5 +1,5 @@
 # accounts/signals.py
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
@@ -9,9 +9,9 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     else:
-        Profile.objects.get_or_create(user=instance)
-        instance.profile.save()
-
+        if hasattr(instance, "profile"):
+            instance.profile.save()
+            
 @receiver(post_delete, sender=User)
 def delete_user_profile(sender, instance, **kwargs):
     try:
